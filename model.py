b2b053c2839b12d15f26bdd2540f79e7ee6f2edf
loup <loup.kreidl@gmail.com>
2018-09-01 21:54:18 +0200

    Cleaner commit

--- model.py
+++ model.py
@@ -1,18 +1,18 @@
 import subprocess
 import json
 
-class Job(object):
 
+class Job(object):
 
     @staticmethod
     def writeDesc(job):
         desc = {
-            'commands':[list(command.split(' ')) for command in job.commands],
+            'commands': [list(command.split(' ')) for command in job.commands],
             'name': job.name,
             'metadata': job.metadata
         }
         return json.dumps(desc)
-    
+
     @staticmethod
     def readDesc(desc):
 
@@ -21,31 +21,28 @@ class Job(object):
         job.addCommands([' '.join(command) for command in desc['commands']])
         return job
 
-
-    def __init__(self, name, metadata = {}):
+    def __init__(self, name, metadata={}):
         self._name = name
         self.commands = []
         if not metadata:
             metadata = {}
         self.metadata = metadata
-    
 
     @property
     def name(self):
         return self._name
 
-
     @name.setter
     def name(self, name):
         self._name = name
-
 
     def addCommands(self, commands):
         if not isinstance(commands, list):
             commands = [commands]
         self.commands.extend(commands)
-    
+
     def execute(self):
-        self.commands = [command.format(**self.metadata) for command in self.commands]
+        self.commands = [command.format(**self.metadata)
+                         for command in self.commands]
         commandLine = '; '.join(self.commands)
         return subprocess.Popen(commandLine, shell=True)
--- worker.py
+++ worker.py
@@ -1,29 +1,33 @@
-class Worker(object):
+import logging
 
+
+class Worker(object):
 
     def __init__(self):
 
-        self.job= None
+        self.job = None
         self.proc = None
 
-
     def addJob(self, job):
-        
-        print('Testing state')
+
         if self.job or (self.proc and self.proc.poll() is None):
             return job
-        
+
         if self.proc:
             self.proc = None
-        
+
         self.job = job
         self.execute()
-
 
     def execute(self):
         if not self.job:
             return
-        
+
         job = self.job
         self.job = None
         self.proc = job.execute()
+
+    def exit(self):
+        self.proc.kill(-9)
+        self.proc = None
+        self.job = None
